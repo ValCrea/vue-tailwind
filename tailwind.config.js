f4eee30cd275e7d112b14167de12d679810bf227
ValCrea <val.waldner@creatim.com>
2022-09-22 11:26:18 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+// Wants to convet to ES6 module
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
