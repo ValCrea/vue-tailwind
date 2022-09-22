5214b58b294e93603aeac13dca7ceaf8137944e7
ValCrea <val.waldner@creatim.com>
2022-09-22 11:27:50 +0200

    group-hover

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="justify-center flex bg-yellow-300 items-center h-screen">
-    <div class="text-4xl">Hello 👋🏼</div>
+  <div className=" flex h-screen justify-center items-center">
+    <div className="group text-xl">
+      <strong className="group-hover:text-red-500">Hover on me </strong>
+      <strong className="group-hover:text-green-500">the texts will be </strong>
+      <strong className="group-hover:text-blue-500">of different colors</strong>
+    </div>
   </div>
 </template>
 
